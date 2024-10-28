225faf58877302ee779b1272d7e529ba5ae425d9
DmitriyYaroshchuk <dima030215@gmail.com>
2024-10-28 15:04:57 +0200

    task-7 changed from method to emulation

--- homework-2/task-7/scripts/script.js
+++ homework-2/task-7/scripts/script.js
@@ -1,8 +1,15 @@
 "use strict";
-const array = [1, 2, 'goods', 'bottle', true];
+const array = ['typescript', 11, false, 'bottle', true, 223, 'javascript'];
+console.log('before reverse array: ', array);
 function reverseArray(arr) {
-    return arr.reverse();
+    for (let i = 0; i < Math.floor(arr.length / 2); i++) {
+        const temp = arr[i];
+        arr[i] = arr[arr.length - 1 - i];
+        arr[arr.length - 1 - i] = temp;
+    }
+    return arr;
 }
 const reversedArr = reverseArray(array);
+console.log('after reverse array: ', array);
 console.log(array === reversedArr);
 //# sourceMappingURL=script.js.map
